d4158a769f1aa0bdbbb75f276a9e0f640c5deaf1
Valentina Guaymas <136923857+Byter-AI@users.noreply.github.com>
2024-10-20 21:13:23 -0300

    Update interaction.js

--- interaction.js
+++ interaction.js
@@ -122,10 +122,4 @@ var TrandingSlider = new Swiper('.tranding-slider', {
     
 });
 
-const cards = document.querySelectorAll('.card');
 
-cards.forEach(card => {
-    card.addEventListener('click', () => {
-        card.querySelector('.card-inner').classList.toggle('flipped');
-    });
-});
